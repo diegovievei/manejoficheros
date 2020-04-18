72856881ed9667bbf7f45a99fd110c0fa0038c69
diegovievei <62600600+diegovievei@users.noreply.github.com>
2020-04-18 12:52:39 +0200

    Add files via upload

--- Manejoficheros.java
+++ Manejoficheros.java
@@ -5,43 +5,30 @@ import java.util.Scanner;
 public class Manejoficheros {
 
       public static void main(String[] args) {
-        Scanner teclado=new Scanner (System.in);
-        String nombreFichero = "E:\\Pandemia\\Programación\\ejemplo.txt";
-        String cr = System.lineSeparator();
-        String frase;
-        try {
-            FileWriter ficherito  = new FileWriter (nombreFichero,true);
-            //FileWriter ficherito = new FileWriter(nombreFichero); // crea nuevo
-            
-            ficherito.write("PRUEBA DE ESCRITURA DE TEXTO" + cr);
-            
-          /* for (int i = 65; i < 91; i++) {
-
-                frase = ("A" + (char) i + cr);
-                ficherito.write(frase);
-
-            }*/
-           /* String nombre= teclado.next();
-            ficherito.write("Hola me llamo "+nombre+cr);
-            int edad= teclado.nextInt();
-            ficherito.write("Tengo "+edad+cr);
-            
-            ficherito.write("FIN  FIN FIN  DE TEXTO" + cr);
-            ficherito.close();
-            System.out.print("CERRAMOS EL FICHERO");*/
-           String nombre=teclado.next();
-           int edad=teclado.nextInt();
-           int altura=teclado.nextInt();
-           
-           Persona p=new Persona(nombre,edad,altura);
-           ficherito.write(p.edad);
-           ficherito.close();
-           System.out.print("CERRAMOS EL FICHERO");
-
-        } catch (IOException ex) {
-            ex.printStackTrace();
-        }
+       Scanner teclado=new Scanner (System.in);
+        String ficheroescritura = "E:\\Pandemia\\Programación\\ejemplo.txt";
+        String ficherolectura = "E:/Pandemia/Programación/ejemplo3.txt";
+//          System.out.println("nombre");
+//        String nombre=teclado.next();
+//          System.out.println("Edad");
+       // int edad=teclado.nextInt();
+         // System.out.println("Altura");
+        //int altura=teclado.nextInt();
+        //Persona p=new Persona(nombre,edad,altura);
+        Persona p2=new Persona("Diego",18,178);
+        Leer_fichero insertarpersona= new Leer_fichero();
+        insertarpersona.escribir2(ficheroescritura, p2);
+        
+//        Leer_fichero escribir= new Leer_fichero();
+        //escribir.escribir(ficheroescritura);
+       
+        //Leer_fichero accediendo= new Leer_fichero();
+        //accediendo.lee(ficherolectura);        
+        
+       
 
     } // fin del main
 
 } // fin de la class y de  todo
+
+
